4f82966c0ab3ba548ac351466c05452c95e45045
YoussefMIbrahim <kn1010az@go.minnstate.edu>
2020-09-01 19:05:43 -0500

    added comments

--- Author_Class.py
+++ Author_Class.py
@@ -2,18 +2,19 @@ class Author:
     def __init__(self, name):
         self.name = name
         self.books = []
-
+    #publish function to add a book to the list
     def publish(self, title):
         if title in self.books:
             print('Sorry that\'s a duplicate')
         else:
             self.books.append(title)
-    # 
+    # string output when you print class
     def __str__(self):
         titles = ', '.join(self.books) or 'No published books'
         return f'{self.name}. Books: {titles}'
 
 def main():
+    # examples using my Author class
     rowling = Author('jk rowling')
     rowling.publish('harry potter')
     rowling.publish('fantastic')
--- student_dataclass.py
+++ student_dataclass.py
@@ -2,12 +2,14 @@ from dataclasses import dataclass
 
 @dataclass
 class Student:
+    # fields will take any value of the set datatype
+    #seems like you can't initilize a field here without giving it a value when you add data to the class
     name: str
     college_id: int
     gpa: float
 
 def main():
-
+    #example data
     quinn = Student('quinn',123145,4)
     bob = Student('bob',1234565,2.3)
     
